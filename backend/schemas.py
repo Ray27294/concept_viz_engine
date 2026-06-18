bf4d61ae87567babd734630abaa728834b795f9e
zz1925 <zz1925@ic.ac.uk>
2026-06-18 16:53:35 +0100

    refactor: refact the class models for metadata

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,21 +1,24 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
-class ForeignKeyInfo(BaseModel):
-    parent_table: str
-    parent_column: str
-    child_table: str
-    child_column: str
-
-class ColumnInfo(BaseModel):
+class Column(BaseModel):
     name: str
     type: str
-    is_primary_key: bool
-    is_foreign_key: bool
     semantic_type: str # numeric, temporal or lexical
+
+class TableColumnGroup(BaseModel):
+    table_name: str
+    columns: List[Column]
+
+class Key(TableColumnGroup):
+    pass
+
+class ForeignKey(BaseModel):
+    referencing: TableColumnGroup
+    referenced: Key
 
 class TableMetadata(BaseModel):
     table_name: str
-    columns: List[ColumnInfo]
-    primary_keys: List[str]
-    foreign_keys: List[ForeignKeyInfo]
+    columns: List[Column]
+    primary_key: Optional[Key]
+    foreign_keys: List[ForeignKey]
